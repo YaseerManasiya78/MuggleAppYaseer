e5197f5adeed9f9d989d98dbd89bffa688cd6ba1
YaseerManasiya78 <yaseermanasiya78@gmail.com>
2024-03-04 02:06:05 +0530

    add validation check in proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "https://api.muggle.link",
-      changeOrigin: true,
-    })
-  );
+  // Check if the environment is not production
+  if (process.env.NODE_ENV !== 'production') {
+    app.use(
+      "/api", // Specify the path that you want to proxy
+      createProxyMiddleware({
+        target: "https://api.muggle.link", // Specify the target URL
+        changeOrigin: true, // Needed for virtual hosted sites
+        // Other optional settings such as headers, etc.
+      })
+    );
+  }
 };
